f943c5f81beb7fc28d90ff5ef0884a63f22b8828
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-04-07 18:29:05 +0900

    Add files via upload

---
+++ motoEvent/script.js
@@ -0,0 +1,30 @@
+const motos = document.querySelectorAll('.moto')
+const motoInnerText = document.querySelector('.moto-inner-text')
+function motoEvent(action, num) {
+  motos.forEach(moto => {
+    if (moto.classList.contains(`m${num}`)) return
+    moto.classList[action](`add${num}`)
+  })
+  if (num === 1) {
+    motoInnerText.style.opacity = 1
+    motoInnerText.textContent = "This is moto1."
+  } else if (num === 2) {
+    motoInnerText.style.opacity = 1
+    motoInnerText.textContent = "This is moto2."
+  } else if (num === 3) {
+    motoInnerText.style.opacity = 1
+    motoInnerText.textContent = "This is moto3."
+  } else if (num === 4) {
+    motoInnerText.style.opacity = 1
+    motoInnerText.textContent = "This is moto4."
+  } else if (num === 5) {
+    motoInnerText.style.opacity = 1
+    motoInnerText.textContent = "This is moto5."
+  }
+  if (action == 'remove') {
+    motoInnerText.style.opacity = 0
+  }
+}
+
+function motoText(num) {
+}
